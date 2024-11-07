adcab81a8956020de5910ba3293f79cf6709038a
benJ <26485097+Mil00Z@users.noreply.github.com>
2024-11-07 17:44:01 +0100

    Get datas From UseOutletCOntext to Home page and Card component props

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,16 +1,18 @@
+
 import {Link} from 'react-router-dom'
 import './Card.css'
 
 
 const Card = (props) => {  
 
-  const {logement} = props
+  const {logement,bookings,setBookings,isBooked, setIsBooked} = props
 
   return (
         
-    <Link to={`/annonce/${logement.id}`}className={`card `} style={{background:`url(${logement.cover}),linear-gradient(to bottom, #FFF 0%, #FFF 42%, #0A0A0A 100%)`}}><h2 className="card-title">{logement.title}</h2>
+    <Link to={`/annonce/${logement.id}`} className={`card ${isBooked && 'booked'}`} style={{background:`url(${logement.cover}),linear-gradient(to bottom, #FFF 0%, #FFF 42%, #0A0A0A 100%)`}}><h2 className="card-title">{logement.title}</h2>
     <p className="card-description">{logement.description}</p></Link>
-    
+  
+  
   )
 
 }
