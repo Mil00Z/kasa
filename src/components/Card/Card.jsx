b6f5e94a346d011059c3e85e7c1027491ad734c1
benJ <26485097+Mil00Z@users.noreply.github.com>
2024-11-07 11:10:57 +0100

    Change Card Html to Global Link item

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -7,12 +7,11 @@ const Card = (props) => {
   const {logement} = props
 
   return (
-    <article className={`card `} style={{background:`url(${logement.cover}),linear-gradient(to bottom, #FFF 0%, #FFF 42%, #0A0A0A 100%)`}}>
-      <Link to={`/annonce/${logement.id}`} className='card-link'><h2 className="card-title">{logement.title}</h2></Link>
-    <p className="card-description">{logement.description}</p></article>         
+        
+    <Link to={`/annonce/${logement.id}`}className={`card `} style={{background:`url(${logement.cover}),linear-gradient(to bottom, #FFF 0%, #FFF 42%, #0A0A0A 100%)`}}><h2 className="card-title">{logement.title}</h2>
+    <p className="card-description">{logement.description}</p></Link>
+    
   )
-
-
 
 }
 
