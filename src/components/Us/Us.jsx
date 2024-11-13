170212dfe11f5cf68c5e02f5abc29ebbd8453264
benJ <26485097+Mil00Z@users.noreply.github.com>
2024-11-13 13:44:20 +0100

    Add fakes description datas beacause missing in project starting files

--- src/components/Us/Us.jsx
+++ src/components/Us/Us.jsx
@@ -4,15 +4,36 @@ import './us.css'
 
 const Us = () =>{
 
-  let softkills = ['Fiabilité','Responsabilite','Respect','Service','Sécurité'];
+  let softskills = [
+    {
+      title: "Fiabilité",
+      description: "La capacité à être fiable et à tenir ses engagements",
+    },
+    {
+      title: "Responsabilité",
+      description: "La capacité à prendre des responsabilités et à assumer ses actes",
+    },
+    {
+      title: "Respect",
+      description: "La capacité à respecter les autres et à être courtois",
+    },
+    {
+      title: "Service",
+      description: "La capacité à respecter les autres et à être courtois",
+    },
+    {
+      title: "Sécurité",
+      description: "La capacité à respecter les autres et à être courtois",
+    }
+  ];
 
   return (
     <section className="us">
       
-      {softkills.map((softkill,index) => {
+      {softskills.map((softskill,index) => {
       
         return(
-          <PanelTube key={`tube-${index}`} softkill={softkill} fullPanel={false} />
+          <PanelTube key={`tube-${index}`} title={softskill.title} description={softskill.description} annoncePanel={false}/>
         )
 
       })}
