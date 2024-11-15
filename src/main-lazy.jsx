61c227fec43a4f3fae80ab84d9c9ea3f5ec96332
benJ <26485097+Mil00Z@users.noreply.github.com>
2024-11-15 14:32:35 +0100

    Testin Lazy Loading Component

---
+++ src/main-lazy.jsx
@@ -0,0 +1,46 @@
+import { StrictMode, Suspense,lazy} from 'react'
+import { createRoot } from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from '@pages/root.jsx';
+import Home from '@pages/home/index.jsx';
+// import About from '@pages/about/index.jsx';
+import Surprise from '@pages/surprise/index.jsx';
+import Annonce from '@pages/annonce/index.jsx';
+import PageNotFound from '@pages/notfound/index.jsx';
+
+const LazyAbout = lazy(() => import('@pages/about/index.jsx'));
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/about",
+        element: <Suspense fallback={<div>Loading...</div>}><LazyAbout /></Suspense>},
+        {
+          path: "/annonce/:id",
+          element: <Annonce />},
+        {
+        path: "/benji",
+        element: <Surprise />},
+        {
+          path: "*",
+          element: <PageNotFound />}
+    ],
+  }
+   
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
